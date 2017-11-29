4bf812f03c7fb3be5a7cdd0b0c959b7eb5334fc2
hacks-KS <b1013027@gmail.com>
2017-11-29 11:21:10 +0900

    Initial Commit

---
+++ app/src/main/java/com/example/b1013027/a1129_task/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.b1013027.a1129_task;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
